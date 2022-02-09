48c1fed72e99720781b8607912f6d09aa4c1c85b
Kanishk <rawat27kanishk@gmail.com>
2022-02-09 19:19:34 +0530

    first commit

---
+++ keyboard.js
@@ -0,0 +1,27 @@
+
+function theme1() {
+    let elem =document.getElementsByTagName('div')
+    elem[1].style.background = "white";
+    document.getElementById("back").style.background ="orange";
+    document.getElementById("esc").style.background ="orange";
+    document.getElementById("space").style.background ="orange";
+    document.getElementById("enter").style.background ="orange";
+}
+function theme2(){
+    let elem =document.getElementsByTagName('div')
+    elem[1].style.background = "aqua";
+    document.getElementById("back").style.background ="pink";
+    document.getElementById("esc").style.background ="pink";
+    document.getElementById("space").style.background ="pink";
+    document.getElementById("enter").style.background ="pink";
+
+}
+function theme3(){
+    let elem =document.getElementsByTagName('div')
+    elem[1].style.background = "black";
+    document.getElementById("back").style.background ="aquamarine";
+    document.getElementById("esc").style.background ="aquamarine";
+    document.getElementById("space").style.background ="aquamarine";
+    document.getElementById("enter").style.background ="aquamarine";
+
+}
